f92c15f7611b380a00c00dcd940b1afc591fa36c
Jun Hyeop <limjunehyeop@gmail.com>
2023-02-28 12:16:43 +0900

    Release automation updates

--- release.sh
+++ release.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-
+./build.sh
+git add ./build/YourSchool.ipa
+git commit -m "rebuild"
 gh release create v"$1" "./build/YourSchool.ipa#" -t "$2" -n "$3" --latest
 rm YourSchool.json
 touch YourSchool.json
-./build.sh
-git add build/YourSchool.ipa
-git commit -m "rebuild"
 python3 release.py "$1" "$3"
 git add YourSchool.json
 git commit -m "Repo Update"
